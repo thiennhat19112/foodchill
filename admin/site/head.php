ec66331fc6cdd12c647a4bedc62ffdc719407fbf
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:32:25 +0700

    Create head.php

---
+++ admin/site/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>FoodChill Dashboard</title>
+  <link rel="stylesheet" href="assets/css/formCss/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/tailwind.output.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+  
+
+</head>
