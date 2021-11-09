bfdb6a6331bad4dc9c02d74b84c91d8fe3e7b588
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:32:28 +0700

    Create footer.php

---
+++ admin/site/footer.php
@@ -0,0 +1,13 @@
+</div>
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+<script src="assets/js/charts-bars.js" defer></script>
+<script src="assets/js/charts-lines.js" defer></script>
+<script src="assets/js/charts-pie.js" defer></script>
+<script src="assets/js/init-alpine.js"></script>
+<script src="assets/js/focus-trap.js" defer></script>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</body>
+
+</html>
