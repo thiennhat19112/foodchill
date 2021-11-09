97d1212a146e586c60fbe2bcf88abd04ace66d61
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:32:56 +0700

    Create edit.php

---
+++ admin/categories/edit.php
@@ -0,0 +1,30 @@
+<div class="col-12 grid-margin stretch-card">
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">Sửa Danh mục</h4>
+      <form class="forms-sample">
+        <div class="form-group">
+          <label for="exampleInputName1">ID danh mục</label>
+          <input type="text" class="form-control" id="exampleInputName1" placeholder="Tên danh mục">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputName1">Tên danh mục</label>
+          <input type="text" class="form-control" id="exampleInputName1" placeholder="Tên danh mục">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail3">Vị trí xuất hiện</label>
+          <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Vị trí xuất hiện">
+        </div>
+        <div class="form-group">
+          <label for="exampleSelectGender">Trạng thái</label>
+          <select class="form-control" id="exampleSelectGender">
+            <option>Hiện</option>
+            <option>Ẩn</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary mr-2">Sửa</button>
+        <a href="?v=categories&act=listCat" class="btn btn-light">Hủy</a>
+      </form>
+    </div>
+  </div>
+</div>
