0e4fb9ecfd0681ea7e40678804a52807159fdd0b
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:32:44 +0700

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<?php
+    require("site/head.php");
+    require("site/sidebar.php");
+    require("site/header.php");
+    if(isset($_GET['v'])) {
+        $v = $_GET['v'];
+        if ($v == "categories"){
+            require("categories/index.php");
+        }else if ($v == "product"){
+            require("products/index.php");
+        }else if ($v == "order"){
+            require("order/order.php");
+        }else if ($v == "user") {
+            require("users/index.php");
+        }else if($v == "comment"){
+            require("comment/comment.php");
+        }
+    }else{
+        require("dashboard/home.php");
+    }
+    require("site/footer.php");
