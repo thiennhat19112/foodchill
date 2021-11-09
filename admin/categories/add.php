1abb5aff4f55b69123450ecf5a0b146244956f43
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:33:00 +0700

    Create add.php

---
+++ admin/categories/add.php
@@ -0,0 +1,26 @@
+<div class="col-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Thêm Danh mục</h4>
+                  <form class="forms-sample">
+                    <div class="form-group">
+                      <label for="exampleInputName1">Tên danh mục</label>
+                      <input type="text" class="form-control" id="exampleInputName1" placeholder="Tên danh mục">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail3">Vị trí xuất hiện</label>
+                      <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Vị trí xuất hiện">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleSelectGender">Trạng thái</label>
+                        <select class="form-control" id="exampleSelectGender">
+                          <option>Hiện</option>
+                          <option>Ẩn</option>
+                        </select>
+                      </div>
+                    <button type="submit" class="btn btn-primary mr-2">Thêm</button>
+                    <a href="?v=categories&act=listCat" class="btn btn-light">Cancel</a>
+                  </form>
+                </div>
+              </div>
+            </div>
