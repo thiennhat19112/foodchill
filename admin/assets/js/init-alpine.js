7c28a805695db2b9c268c66b388bc9f752a56131
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:33:02 +0700

    Create init-alpine.js

---
+++ admin/assets/js/init-alpine.js
@@ -0,0 +1,88 @@
+function data() {
+  function getThemeFromLocalStorage() {
+    // if user already changed the theme, use it
+    if (window.localStorage.getItem('dark')) {
+      return JSON.parse(window.localStorage.getItem('dark'))
+    }
+
+    // else return their preferences
+    return (
+      !!window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    )
+  }
+
+  function setThemeToLocalStorage(value) {
+    window.localStorage.setItem('dark', value)
+  }
+
+  return {
+    dark: getThemeFromLocalStorage(),
+    toggleTheme() {
+      this.dark = !this.dark
+      setThemeToLocalStorage(this.dark)
+    },
+    isSideMenuOpen: false,
+    toggleSideMenu() {
+      this.isSideMenuOpen = !this.isSideMenuOpen
+    },
+    closeSideMenu() {
+      this.isSideMenuOpen = false
+    },
+    isNotificationsMenuOpen: false,
+    toggleNotificationsMenu() {
+      this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen
+    },
+    closeNotificationsMenu() {
+      this.isNotificationsMenuOpen = false
+    },
+    isProfileMenuOpen: false,
+    toggleProfileMenu() {
+      this.isProfileMenuOpen = !this.isProfileMenuOpen
+    },
+    closeProfileMenu() {
+      this.isProfileMenuOpen = false
+    },
+
+    isPostMenuOpen: false,
+    togglePostOpen() {
+      this.isPostMenuOpen = !this.isPostMenuOpen
+    },
+
+    isPagesCategoriesOpen: false,
+    toggleCategoriesMenu() {
+      this.isPagesCategoriesOpen = !this.isPagesCategoriesOpen
+    },
+
+    isProductsMenuOpen: false,
+    toggleProductsMenu() {
+      this.isProductsMenuOpen = !this.isProductsMenuOpen
+    },
+
+    isUserMenuOpen: false,
+    toggleUsersMenu() {
+      this.isUserMenuOpen = !this.isUserMenuOpen
+    },
+
+    isPagesMenuOpen: false,
+    togglePagesMenu() {
+      this.isPagesMenuOpen = !this.isPagesMenuOpen
+    },
+    
+    isPagesMenuOpen: false,
+    togglePagesMenu() {
+      this.isPagesMenuOpen = !this.isPagesMenuOpen
+    },
+    // Modal
+    isModalOpen: false,
+    trapCleanup: null,
+    openModal() {
+      this.isModalOpen = true
+      this.trapCleanup = focusTrap(document.querySelector('#modal'))
+    },
+    closeModal() {
+      this.isModalOpen = false
+      this.trapCleanup()
+    },
+  }
+}
