4fe65140aa2ba9d8f19d8b3ad8036a42f2dad06d
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-11-09 14:32:09 +0700

    Create list.php

---
+++ admin/users/list.php
@@ -0,0 +1,129 @@
+<div class="container px-6 mx-auto grid">
+    <div class="w-full overflow-hidden rounded-lg whitespace-wrap">
+        <div class="w-full overflow-x-auto">
+            <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                Khách hàng
+            </h2>
+            <table class="w-full whitespace-wrap">
+                <thead>
+                    <tr class="text-xs font-semibold whitespace-no-wrap tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                        <th class="px-3 py-3">Tên Khách hàng</th>
+                        <th class="px-3 py-3">Số điện thoại</th>
+                        <th class="px-3 py-3">Email</th>
+                        <th class="px-3 py-3">Địa chỉ</th>
+                        <th class="px-3 py-3">Tài khoảng</th>
+                        <th class="px-3 py-3">Phân quyền</th>
+                        <th class="px-3 py-3">Thao tác</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                    <tr class="text-gray-700 dark:text-gray-400">
+                        <td class="px-3 py-3">
+                            <div class="flex items-center text-sm">
+                                <!-- Avatar with inset shadow -->
+                                <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                    <img class="object-cover w-full h-full rounded-full" src="https://th.bing.com/th/id/R.fdcd586951c6c0f0a06a04649cbd39b7?rik=X6cQuOF31jQPPw&riu=http%3a%2f%2fhurimex.com.vn%2fdata%2fnews%2f1477%2ftoi2.jpg&ehk=wIfSr1SbyfGzkhuA%2bDeK34VNPP%2f4N5%2bYWM3LNiDWgG0%3d&risl=&pid=ImgRaw&r=0" alt="" loading="lazy" />
+                                    <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                                </div>
+                                <div>
+                                    <p class="font-semibold">Củ tỏi</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="px-3 py-3 text-sm">
+                            0900
+                        </td>
+                        <td class="px-3 py-3 whitespace-wrap">
+                            Lorem ipsum dolor sit amet.
+                        </td>
+                        <td class="px-3 py-3 whitespace-wrap" >
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, maiores! </td>
+                        <td class="px-3 py-3 text-sm">
+                            6/10/2020
+                        </td>
+                        <td class="px-3 py-3 text-xs">
+                            <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                Hiện
+                            </span>
+                        </td>
+
+                        <td class="px-3 py-3">
+                            <div class="flex items-center space-x-4 text-sm">
+                                <a href="?v=user&act=editUser"class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                                    </svg>
+                                </a>
+                                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="grid px-3 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+            <span class="flex items-center col-span-3">
+                Showing 21-30 of 100
+            </span>
+            <span class="col-span-2"></span>
+            <!-- Pagination -->
+            <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+                <nav aria-label="Table navigation">
+                    <ul class="inline-flex items-center">
+                        <li>
+                            <button class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple" aria-label="Previous">
+                                <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                    <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                1
+                            </button>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                2
+                            </button>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 text-white transition-colors duration-150 bg-purple-600 border border-r-0 border-purple-600 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                3
+                            </button>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                4
+                            </button>
+                        </li>
+                        <li>
+                            <span class="px-3 py-1">...</span>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                8
+                            </button>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">
+                                9
+                            </button>
+                        </li>
+                        <li>
+                            <button class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple" aria-label="Next">
+                                <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                    <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </span>
+        </div>
+    </div>
+</div>
