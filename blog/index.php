78e740d8e4ecbb135fff8ecb33b6bb91603c901f
Trình Thanh Phong <dinhmenhhathui@gmail.com>
2021-11-11 11:09:14 +0700

    upload file

---
+++ blog/index.php
@@ -0,0 +1,6 @@
+<?php
+   require("../views/public/header.php");
+   require("../views/public/navbar.php");
+   require("../views/blog.php");
+   require("../views/public/footer.php");
+?>
---
+++ views/contact.php
@@ -0,0 +1,102 @@
+   
+   
+   <!-- Breadcrumb Section Begin -->
+      <section class="breadcrumb-section set-bg" data-setbg="../assets/img/breadcrumb.jpg">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-12 text-center">
+                  <div class="breadcrumb__text">
+                     <h2>Liên hệ với chúng tôi</h2>
+                     <div class="breadcrumb__option">
+                        <a href="../home">Trang chủ</a>
+                        <span>Liên hệ với chúng tôi</span>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   <!-- Breadcrumb Section End -->
+
+   <!-- Contact Section Begin -->
+      <section class="contact spad">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                  <div class="contact__widget">
+                     <span class="icon_phone"></span>
+                     <h4>Điện thoại</h4>
+                     <p>+01-3-8888-6868</p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                  <div class="contact__widget">
+                     <span class="icon_pin_alt"></span>
+                     <h4>Địa chỉ</h4>
+                     <p>60-49 Road 11378 New York</p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                  <div class="contact__widget">
+                     <span class="icon_clock_alt"></span>
+                     <h4>Giờ mở cửa</h4>
+                     <p>10:00 am to 23:00 pm</p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                  <div class="contact__widget">
+                     <span class="icon_mail_alt"></span>
+                     <h4>Email</h4>
+                     <p>hello@colorlib.com</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   <!-- Contact Section End -->
+
+   <!-- Map Begin -->
+      <div class="map">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.4552263292267!2d106.62735611476465!3d10.852939092269615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529b6a2b351a5%3A0x11690ada8c36f9bc!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIFRo4buxYyBow6BuaCBGUFQgUG9seXRlY2huaWMgVFAuSENNIChDUzMp!5e0!3m2!1svi!2s!4v1636365021057!5m2!1svi!2s" 
+            height="500" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" loading="lazy"></iframe>
+         
+         <!-- <div class="map-inside">
+            <i class="icon_pin"></i>
+            <div class="inside-widget">
+                  <h4>New York</h4>
+                  <ul>
+                     <li>Phone: +12-345-6789</li>
+                     <li>Add: 16 Creek Ave. Farmingdale, NY</li>
+                  </ul>
+            </div>
+         </div> -->
+      </div>
+   <!-- Map End -->
+
+   <!-- Contact Form Begin -->
+      <div class="contact-form spad">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="contact__form__title">
+                     <h2>Để lại lời nhắn</h2>
+                  </div>
+               </div>
+            </div>
+            <form action="#">
+               <div class="row">
+                  <div class="col-lg-6 col-md-6">
+                     <input type="text" placeholder="Tên của bạn">
+                  </div>
+                  <div class="col-lg-6 col-md-6">
+                     <input type="text" placeholder="Email">
+                  </div>
+                  <div class="col-lg-12 text-center">
+                     <textarea placeholder="Lời nhắn"></textarea>
+                     <button type="submit" class="site-btn">Gửi</button>
+                  </div>
+               </div>
+            </form>
+         </div>
+      </div>
+   <!-- Contact Form End -->
