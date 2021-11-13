f5e457fd89badc65f7f33d90920e7401c472f92a
cuongha0701 <cuongha0701@gmail.com>
2021-11-13 19:10:43 +0700

    login form

---
+++ login/forgot-password.php
@@ -0,0 +1,63 @@
+﻿<!doctype html>
+<html class="no-js " lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+
+    <title>:: FOODCHILL :: QUÊN MẬT KHẨU</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.min.css">
+</head>
+
+<body class="theme-blush">
+
+    <div class="authentication">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-12">
+                    <form class="card auth_form">
+                        <div class="header">
+                            <img class="logo" src="../assets/img/logo.png" alt="">
+                            <h5>Quên mật khẩu?</h5>
+                            <span>Nhập email của bạn để khôi phục mật khẩu.</span>
+                        </div>
+                        <div class="body">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Nhập email">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="zmdi zmdi-email"></i></span>
+                                </div>
+                            </div>
+                            <a href="index.html" class="btn btn-primary btn-block waves-effect waves-light">XÁC NHẬN</a>
+                            <div class="signin_with mt-3">
+                                <a href="javascript:void(0);" class="link">Cần trợ giúp?</a>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- <div class="copyright text-center">
+                    &copy;
+                    <script>document.write(new Date().getFullYear())</script>,
+                    <span>Designed by <a href="https://thememakker.com/" target="_blank">ThemeMakker</a></span>
+                </div> -->
+                </div>
+                <div class="col-lg-8 col-sm-12">
+                    <div class="card">
+                        <img src="../assets/img/signin.svg" alt="Forgot Password" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery Core Js -->
+    <script src="../assets/scripts/libscripts.bundle.js"></script>
+    <script src="../assets/scripts/vendorscripts.bundle.js"></script>
+</body>
+
+</html>
---
+++ login/sign-up.php
@@ -0,0 +1,88 @@
+﻿<!doctype html>
+<html class="no-js " lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+
+    <title>:: FOODCHILL :: ĐĂNG KÝ</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.min.css">
+</head>
+
+<body class="theme-blush">
+
+    <div class="authentication">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-12">
+                    <form class="card auth_form">
+                        <div class="header">
+                            <img class="logo" src="../assets/img/logo.png" alt="">
+                            <!-- <img class="logo" src="./logo.png" alt=""> -->
+                            <h5>Đăng ký</h5>
+                            <span>Đăng ký thành viên mới</span>
+                        </div>
+                        <div class="body">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Tên người dùng">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
+                                </div>
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Hộp thư">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="zmdi zmdi-email"></i></span>
+                                </div>
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Mật khẩu">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="zmdi zmdi-lock"></i></span>
+                                </div>
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Nhập lại mật khẩu">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="zmdi zmdi-lock"></i></span>
+                                </div>
+                            </div>
+                            <div class="checkbox">
+                                <input id="remember_me" type="checkbox">
+                                <label for="remember_me">Đồng ý với <a href="javascript:void(0);">điều khoản sử
+                                        dụng</a></label>
+                            </div>
+                            <a href="index.html" class="btn btn-primary btn-block waves-effect waves-light">ĐĂNG KÝ</a>
+                            <div class="signin_with mt-3">
+                                <a class="link" href="sign-in.html">Bạn đã có tài khoản?</a>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- <div class="copyright text-center">
+                    &copy;
+                    <script>document.write(new Date().getFullYear())</script>,
+                    <span>Designed by <a href="https://thememakker.com/" target="_blank">ThemeMakker</a></span>
+                </div> -->
+                </div>
+                <div class="col-lg-8 col-sm-12">
+                    <div class="card">
+                        <img src="../assets/img/signup.svg" alt="Sign Up" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Jquery Core Js -->
+    <script src="../assets/scripts/libscripts.bundle.js"></script>
+    <script src="../assets/scripts/vendorscripts.bundle.js"></script>
+</body>
+
+</html>
