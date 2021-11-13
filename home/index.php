2c38adea59111ca0feaafe99a152d5cb4cfc586c
Trình Thanh Phong <57865067+BotDotZero@users.noreply.github.com>
2021-11-13 18:45:01 +0700

    Update index.php

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
    require("../views/public/navbar.php");
    require_once("../models/pdo.php");
    require_once("../models/category.php");
-   // require_once("../models/product.php");
+   require_once("../models/product.php");
    $cates = getAllCate();
    require("../views/home.php");
    require("../views/public/footer.php");
