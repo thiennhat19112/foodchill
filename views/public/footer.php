a209baadc32e07acc8e980547636fe0aa996e867
Trình Thanh Phong <dinhmenhhathui@gmail.com>
2021-11-15 15:29:08 +0700

    update footer

--- views/public/footer.php
+++ views/public/footer.php
@@ -13,6 +13,14 @@
                         <li>Email: contact@example.com</li>
                      </ul>
                   </div>
+                  <div class="footer__widget">
+                     <div class="footer__widget__social">
+                        <a href="#1" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="#1" target="_blank"><i class="fa fa-instagram"></i></a>
+                        <a href="#1" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="#1" target="_blank"><i class="fa fa-pinterest"></i></a>
+                     </div>
+                  </div>
                </div>
                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
                   <div class="footer__widget">
@@ -36,25 +44,7 @@
                   </div>
                </div>
                <div class="col-lg-4 col-md-12">
-                  <div class="footer__widget">
-                     <h6>Tham gia bản tin của chúng tôi ngay bây giờ</h6>
-                     <p>Nhận thông tin cập nhật qua Email về sản phẩm mới nhất của chúng tôi và các ưu đãi đặc biệt.</p>
-                     <form action="#">
-                        <input type="text" placeholder="Email của bạn">
-                        <button type="submit" class="site-btn">Đăng ký</button>
-                     </form>
-                     <div class="footer__widget__social">
-                        <a href="#1" target="_blank"><i class="fa fa-facebook"></i></a>
-                        <a href="#1" target="_blank"><i class="fa fa-instagram"></i></a>
-                        <a href="#1" target="_blank"><i class="fa fa-twitter"></i></a>
-                        <a href="#1" target="_blank"><i class="fa fa-pinterest"></i></a>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-lg-12">
-                  <div class="footer__copyright">
+               <div class="footer__copyright">
                      <div class="footer__copyright__text"><p>
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
