e65e28eb5443999cb89549fababd451db3c8530e
Trình Thanh Phong <dinhmenhhathui@gmail.com>
2021-11-15 16:13:08 +0700

    Update product.php

--- models/product.php
+++ models/product.php
@@ -3,6 +3,10 @@
       $sql = "SELECT * FROM `products`";
       return pdo_query($sql);
    }
+   function getProd($p_id){
+      $sql = "SELECT * FROM `products` WHERE `product_id`= ?";
+      return pdo_query($sql, $p_id);
+   }
    function getDiscountProd(){
       $sql = "SELECT * FROM `products` WHERE `discount` > 0";
       return pdo_query($sql);
