26ec469bde86c1b3143fa8c2c34c04e7e85e508f
cuongha0701 <cuongha0701@gmail.com>
2021-11-19 22:28:13 +0700

    Sua loi trang chi tiet san pham

--- product/index.php
+++ product/index.php
@@ -1,9 +1,2 @@
 <?php
-require_once("../models/pdo.php");
-require_once("../models/category.php");
-require_once("../models/product.php");
-require("../views/public/header.php");
-require("../views/public/navbar.php");
 $prod = getProd($_GET["prod_id"]);
-require_once("../views/product.php");
-require("../views/public/footer.php");
