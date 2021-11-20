77913c0aa4256ceae8406e93196dd60eb28ee384
cuongha0701 <cuongha0701@gmail.com>
2021-11-20 10:44:31 +0700

    Sua duong dan khi dang xuat tren admin

--- admin/index.php
+++ admin/index.php
@@ -2,10 +2,10 @@
 session_start();
 if (isset($_SESSION["username"])) {
     if ($_SESSION["phanquyen"] != '1') {
-        header("location: ../home");
+        header("location: ../");
     }
 } else {
-    header("location: ../home");
+    header("location: ../login");
 }
 
 if (isset($_GET['v'])) {
