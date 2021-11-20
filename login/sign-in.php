388ddc099d9353ce081148d8bb4b727e4f4cf884
cuongha0701 <cuongha0701@gmail.com>
2021-11-20 19:01:53 +0700

    Rewrite Url

--- login/sign-in.php
+++ login/sign-in.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
-
+    <!-- <base href="/mywebsite/foodchill/login"> -->
     <title>:: FOODCHILL :: ĐĂNG NHẬP</title>
     <!-- Favicon-->
     <!-- <link rel="icon" href="favicon.ico" type="image/x-icon"> -->
--- views/public/header.php
+++ views/public/header.php
@@ -7,6 +7,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="description" content="FoodChill">
    <meta name="keywords" content="FoodChill, Frozen Food">
+   <base href="/mywebsite/foodchill/">
    <title>FoodChill</title>
 
    <!-- Css Styles -->
