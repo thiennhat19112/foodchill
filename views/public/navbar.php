50dbc37073938f78f7193d8ce37ce75d069aad7b
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2021-12-04 22:48:46 +0700

    update interface

--- views/public/navbar.php
+++ views/public/navbar.php
@@ -42,8 +42,8 @@
    </div>
    <nav class="humberger__menu__nav mobile-menu">
       <ul>
-         <li class="active"><a href="home/">Home</a></li>
-         <li><a href="shop/">Shop</a></li>
+         <li class="active"><a href="home/">Trang chủ/a></li>
+         <li><a href="shop/">Sản phẩm</a></li>
          <li><a href="blog/">Bài viết</a></li>
          <li><a href="contact/">Liên hệ</a></li>
       </ul>
@@ -134,8 +134,8 @@
             <nav class="header__menu">
                <ul>
                   <!-- <li class="active"><a href="home/">Home</a></li> -->
-                  <li class="<?php echo $act == "home" ? 'active' : '' ?>"><a href="home/">Home</a></li>
-                  <li class="<?php echo $act == "shop" || $act == "prod_detail" ? 'active' : '' ?>"><a href="shop/">Shop</a></li>
+                  <li class="<?php echo $act == "home" ? 'active' : '' ?>"><a href="home/">Trang chủ</a></li>
+                  <li class="<?php echo $act == "shop" || $act == "prod_detail" ? 'active' : '' ?>"><a href="shop/">Sản phẩm</a></li>
                   <!-- <li><a href="#">Pages</a>
                         <ul class="header__menu__dropdown">
                            <li><a href="./shop-details.html">Shop Details</a></li>
@@ -144,7 +144,7 @@
                            <li><a href="./blog-details.html">Blog Details</a></li>
                         </ul>
                      </li> -->
-                  <li class="<?php echo $act == "blog" || $act == "blog_detail" ? 'active' : '' ?>"><a href="blog/">Blog</a></li>
+                  <li class="<?php echo $act == "blog" || $act == "blog_detail" ? 'active' : '' ?>"><a href="blog/">Tin tức</a></li>
                   <li class="<?php echo $act == "contact" ? 'active' : '' ?>"><a href="contact/">Liên hệ</a></li>
                </ul>
             </nav>
